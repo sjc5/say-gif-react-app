cb61669d384daaf7d7613fee2ca55f39e7f93e22
sjc <email@sjc.sh>
2021-11-20 20:41:58 -0600

    saving progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  return <div className="App">Ethereum Smart Contract Frontend</div>;
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("make sure you have metamask!");
+        return;
+      } else {
+        console.log("we have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("no authorized account found");
+      }
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="App">Ethereum Smart Contract Frontend {currentAccount}</div>
+  );
 }
 
 export default App;
